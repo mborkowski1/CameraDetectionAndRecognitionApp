20598c454cba514f96128a097f07e93f3d45c8a2
Michał Borkowski <borkowski120@wp.pl>
2018-06-18 02:36:36 +0200

    Add object detection

--- app/main.py
+++ app/main.py
@@ -26,6 +26,9 @@ class Qt5Cam(QDialog):
         self.faceDetectionButton.toggled.connect(self.detect_webcam_face)
         self.face_Enabled = False
 
+        self.objectDetectionButton.setCheckable(True)
+        self.objectDetectionButton.toggled.connect(self.detect_webcam_object)
+        self.object_Enabled = False
 
     def update_frame(self):
         ret, self.image = self.capture.read()
@@ -35,6 +38,12 @@ class Qt5Cam(QDialog):
         if self.face_Enabled:
             detected_image = self.detect_face(self.image)
             self.display_image(detected_image, 1)
+        else:
+            self.display_image(self.image, 1)
+
+        if self.object_Enabled:
+            detected_image2 = self.detect_object(self.image)
+            self.display_image(detected_image2, 1)
         else:
             self.display_image(self.image, 1)
 
@@ -64,6 +73,14 @@ class Qt5Cam(QDialog):
             self.faceDetectionButton.setText('Detect Face And Eye')
             self.face_Enabled = False
 
+    def detect_webcam_object(self, status):
+        if status:
+            self.objectDetectionButton.setText('Stop Detection')
+            self.object_Enabled = True
+        else:
+            self.objectDetectionButton.setText('Detect Object')
+            self.object_Enabled = False
+
     def detect_face(self, img):
         gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
         faces = self.faceCascade.detectMultiScale(gray, 1.3, 5)
@@ -77,6 +94,32 @@ class Qt5Cam(QDialog):
 
             for (ex, ey, ew, eh) in eyes:
                 cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+
+        return img
+
+    def detect_object(self, img):
+        classes = ["background", "aeroplane", "bicycle", "bird", "boat",
+                   "bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
+                   "dog", "horse", "motorbike", "person", "pottedplant", "sheep",
+                   "sofa", "train", "tvmonitor"]
+        colors = np.random.uniform(0, 255, size=(len(classes), 3))
+        net = cv2.dnn.readNetFromCaffe("MobileNetSSD_deploy.prototxt", "MobileNetSSD_deploy.caffemodel")
+
+        (h, w) = img.shape[:2]
+        blob = cv2.dnn.blobFromImage(cv2.resize(img, (300, 300)), 0.007843, (300, 300), 127.5)
+        net.setInput(blob)
+        detections = net.forward()
+
+        for i in np.arange(0, detections.shape[2]):
+            confidence = detections[0, 0, i, 2]
+            if confidence > 0.2:
+                idx = int(detections[0, 0, i, 1])
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                label = "{}: {:.2f}%".format(classes[idx], confidence * 100)
+                cv2.rectangle(img, (startX, startY), (endX, endY), colors[idx], 2)
+                y = startY - 15 if startY - 15 > 15 else startY + 15
+                cv2.putText(img, label, (startX, y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, colors[idx], 2)
 
         return img
 
